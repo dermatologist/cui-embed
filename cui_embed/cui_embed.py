63aca3ccf5955968615bf2f42625847d09521d0b
Bell Eapen <github@gulfdoctor.net>
2023-04-13 20:43:45 -0400

    icd in progresss

--- cui_embed/cui_embed.py
+++ cui_embed/cui_embed.py
@@ -4,13 +4,15 @@ import zipfile
 import tempfile
 from gensim.models import KeyedVectors
 from gensim.scripts.glove2word2vec import glove2word2vec
+import json
 
 class Cuimodel(object):
 
-    def __init__(self, model="cui"):
+    def __init__(self, model="cui", path='data/embeddings.json'):
         super().__init__()
         self.model_directory = tempfile.gettempdir()
         self.model_type = model
+        self.path = path
         if model == "cui":
             self.model_url = 'https://ndownloader.figshare.com/files/10959626'
             self.model_name = 'cui2vec_gensim.bin'
@@ -19,6 +21,9 @@ class Cuimodel(object):
             self.model_url = "https://github.com/elleros/DSHealth2019_loinc_embeddings/raw/master/Data/loinc_s200_w5_c5_sg_wv.txt"
             self.model_name = 'loinc2vec_gensim.bin'
             self.model_text = 'loinc2vec_g.txt'
+        elif model == "icd":
+            self.model_name = 'icd2vec_gensim.bin'
+            self.model_text = 'icd2vec_g.txt'
         else:
             raise ValueError("Model not found")
 
@@ -54,7 +59,25 @@ class Cuimodel(object):
                 urllib.request.urlretrieve(url, os.path.join(
                     self.model_directory, self.model_text))
                 print('This may take some time')
-
+            elif self.model_type == "icd":
+                with open(self.path, 'r') as f:
+                    data = json.load(f)
+                print('No model found. Downloading model...')
+                print('This may take some time')
+                print('Converting model ...')
+                for k, embedding in data.items():
+                    # line = k + ' ' + ' '.join(v) + ' \n'
+                    # f1.write(line)
+                    # with open('data/' + k + '.txt', 'w') as f1:
+                    #     for term, weights in embedding.items():
+                    #         line = term + ' ' + ' '.join(weights) + ' \n'
+                    #         f1.write(line)
+                    if k == 'CBOW_CO_embeddings_100':
+                        with open(os.path.join(self.model_directory, self.model_text), 'w') as f2:
+                            f2.write(str(len(embedding)) + ' ' + '100' + ' \n')
+                            for term, weights in embedding.items():
+                                line = term + ' ' + ' '.join(weights) + ' \n'
+                                f2.write(line)
 
             wv_from_text = KeyedVectors.load_word2vec_format(os.path.join(
                 self.model_directory, self.model_text), unicode_errors='ignore')
