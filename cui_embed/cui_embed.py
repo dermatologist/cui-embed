2a855e48e75ea01989c97deede05c44c027fc258
Bell Eapen <github@gulfdoctor.net>
2023-11-13 14:49:54 -0600

    Add FastText to CuiModel for clinical embeddings

--- cui_embed/cui_embed.py
+++ cui_embed/cui_embed.py
@@ -2,7 +2,7 @@ import os
 import urllib.request
 import zipfile
 import tempfile
-from gensim.models import KeyedVectors
+from gensim.models import KeyedVectors, FastText
 from gensim.scripts.glove2word2vec import glove2word2vec
 
 class Cuimodel(object):
@@ -21,51 +21,63 @@ class Cuimodel(object):
             self.model_text = 'loinc2vec_g.txt'
         elif model == "clinical":
             self.model_url = "https://huggingface.co/garyw/clinical-embeddings-300d-ft-cr/resolve/main/ft_oa_corp_300d.bin?download=true"
-            self.model_name = 'clinical2vec_gensim.bin'
-            self.model_text = 'clinical2vec_g.txt'
+            self.model_name = ['ft_oa_corp_300d.bin', 'ft_oa_corp_300d.bin.wv.vectors.npy']
+            self.model_text = ''
         else:
             raise ValueError("Model not found")
 
-    def model(self):
-        model_exists = os.path.exists(os.path.join(
-            self.model_directory, self.model_name))
-        if model_exists is not True:
-            if self.model_type == "cui":
-                url = self.model_url
-                print('No model found. Downloading model...')
-                urllib.request.urlretrieve(url, os.path.join(self.model_directory, 'cui2vec_pretrained.csv.zip'))
-                print('This may take some time')
-                print('Unzipping model...')
-                with zipfile.ZipFile(os.path.join(self.model_directory, 'cui2vec_pretrained.csv.zip'), "r") as zip_ref:
-                    zip_ref.extractall(self.model_directory)
-                print('Processing model...')
-                with open(os.path.join(self.model_directory, 'cui2vec_pretrained.csv'), 'r') as f:
-                    with open(os.path.join(self.model_directory, self.model_text), 'w') as f1:
-                        next(f)  # skip header line
-                        count = 0
-                        for line in f:
-                            line = '"'.join(line.split()).replace('"', '')
-                            line = ",".join(line.split()).replace(',', ' ')
-                            line = line + ' \n'
-                            f1.write(line)
-                            count += 1
-                print('Converting model ...')
-                glove2word2vec(os.path.join(self.model_directory, self.model_text), os.path.join(
-                    self.model_directory, self.model_text))
-            elif self.model_type == "loinc" or self.model_type == "clinical":
-                url = self.model_url
-                print('No model found. Downloading model...')
-                urllib.request.urlretrieve(url, os.path.join(
-                    self.model_directory, self.model_text))
-                print('This may take some time')
-
-
+    def load_model(self):
+        if self.model_type == "cui":
+            url = self.model_url
+            print('No model found. Downloading model...')
+            urllib.request.urlretrieve(url, os.path.join(self.model_directory, 'cui2vec_pretrained.csv.zip'))
+            print('This may take some time')
+            print('Unzipping model...')
+            with zipfile.ZipFile(os.path.join(self.model_directory, 'cui2vec_pretrained.csv.zip'), "r") as zip_ref:
+                zip_ref.extractall(self.model_directory)
+            print('Processing model...')
+            with open(os.path.join(self.model_directory, 'cui2vec_pretrained.csv'), 'r') as f:
+                with open(os.path.join(self.model_directory, self.model_text), 'w') as f1:
+                    next(f)  # skip header line
+                    count = 0
+                    for line in f:
+                        line = '"'.join(line.split()).replace('"', '')
+                        line = ",".join(line.split()).replace(',', ' ')
+                        line = line + ' \n'
+                        f1.write(line)
+                        count += 1
+            print('Converting model ...')
+            glove2word2vec(os.path.join(self.model_directory, self.model_text), os.path.join(
+                self.model_directory, self.model_text))
+        elif self.model_type == "loinc":
+            url = self.model_url
+            print('No model found. Downloading model...')
+            urllib.request.urlretrieve(url, os.path.join(
+                self.model_directory, self.model_text))
+            print('This may take some time')
             wv_from_text = KeyedVectors.load_word2vec_format(os.path.join(
                 self.model_directory, self.model_text), unicode_errors='ignore')
             wv_from_text.save_word2vec_format(os.path.join(
                 self.model_directory, self.model_name), binary=True)
             print('Model processing completed ..')
-        return KeyedVectors.load_word2vec_format(os.path.join(self.model_directory, self.model_name), unicode_errors='ignore', binary=True)
+
+        if self.model_type == "clinical":
+            url = self.model_url
+            print('No model found. Downloading model...')
+            for _model in self.model_name:
+                urllib.request.urlretrieve(url, os.path.join(
+                    self.model_directory, _model))
+            print('This may take some time')
+
+
+    def model(self):
+        model_exists = os.path.exists(self.model_directory)
+        if model_exists is not True:
+            self.load_model()
+        if self.model_type != "clinical":
+            return KeyedVectors.load_word2vec_format(os.path.join(self.model_directory, self.model_name), unicode_errors='ignore', binary=True)
+        else:
+            return FastText.load(os.path.join(self.model_directory, self.model_name[0]))
 
 @staticmethod
 def model(self):
--- tests/test_cui_embed.py
+++ tests/test_cui_embed.py
@@ -1,16 +1,24 @@
 import pytest
 
-def test_cui_model():
-    from cui_embed import Cuimodel
-    cm = Cuimodel('cui')
-    m = cm.model()
-    # 'C0002268'
-    assert len(m.most_similar_cosmul(['C0002268'])) > 0
+# def test_cui_model():
+#     from cui_embed import Cuimodel
+#     cm = Cuimodel('cui')
+#     m = cm.model()
+#     # 'C0002268'
+#     assert len(m.most_similar_cosmul(['C0002268'])) > 0
 
 
-def test_loinc_model():
+# def test_loinc_model():
+#     from cui_embed import Cuimodel
+#     cm = Cuimodel('loinc')
+#     m = cm.model()
+#     # LOINC Code 2324-2 Gamma glutamyl transferase
+#     assert len(m.most_similar_cosmul(['2324-2'])) > 0
+#     print(m.most_similar_cosmul(['2324-2']))
+
+def test_clinical_model():
     from cui_embed import Cuimodel
-    cm = Cuimodel('loinc')
+    cm = Cuimodel('clinical')
     m = cm.model()
     # LOINC Code 2324-2 Gamma glutamyl transferase
     assert len(m.most_similar_cosmul(['2324-2'])) > 0
